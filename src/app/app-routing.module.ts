095960f9d5242da034b5200a07d1e9b21630d307
Rodrigo <kauarodrigo1193@gmail.com>
2024-02-27 00:27:24 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { ServicesCardComponent } from './pages/services/services-card.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'services', component: ServicesCardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
